5645c3dbcdba60d65bd69e7e2385fa0eb9650e90
saidureg <saidureg@gmail.com>
2024-05-27 23:44:46 +0600

    used trigger scrub start end in scrollTrigger

--- ScrollTrigger/script.js
+++ ScrollTrigger/script.js
@@ -1,14 +1,42 @@
-gsap.from("#page-1 #box", {
-  scale: 0,
+// gsap.from("#page-1 #box", {
+//   scale: 0,
+//   duration: 2,
+//   delay: 2,
+//   rotation: 360,
+// });
+
+// gsap.from("#page-2 #box", {
+//   scale: 0,
+//   duration: 2,
+//   rotation: 360,
+//   scrollTrigger: {
+//     trigger: "#page-2 #box",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 80%",
+//   },
+// });
+
+gsap.from("#page-3 h1", {
+  opacity: 0,
   duration: 2,
-  delay: 2,
-  rotation: 360,
+  x: -200,
+  scrollTrigger: {
+    trigger: "#page-3 h1",
+    scroller: "body",
+    markers: true,
+    start: "top 60%",
+  },
 });
 
-gsap.from("#page-2 #box", {
-  scale: 0,
+gsap.from("#page-3 h2", {
+  opacity: 0,
   duration: 2,
-  delay: 1,
-  rotation: 360,
-  scrollTrigger: "#page-2 #box",
+  x: 200,
+  scrollTrigger: {
+    trigger: "#page-3 h2",
+    scroller: "body",
+    markers: true,
+    start: "top 60%",
+  },
 });
