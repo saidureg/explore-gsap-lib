0ca0889e10bcd7d68f38094cb0c7b6a9f86046e1
saidureg <saidureg@gmail.com>
2024-05-28 00:24:28 +0600

    used transform pin scrub in scrollTrigger

--- ScrollTrigger/script.js
+++ ScrollTrigger/script.js
@@ -1,21 +1,22 @@
-// gsap.from("#page-1 #box", {
-//   scale: 0,
-//   duration: 2,
-//   delay: 2,
-//   rotation: 360,
-// });
+gsap.from("#page-1 #box", {
+  scale: 0,
+  duration: 2,
+  delay: 2,
+  rotation: 360,
+});
 
-// gsap.from("#page-2 #box", {
-//   scale: 0,
-//   duration: 2,
-//   rotation: 360,
-//   scrollTrigger: {
-//     trigger: "#page-2 #box",
-//     scroller: "body",
-//     markers: true,
-//     start: "top 80%",
-//   },
-// });
+gsap.to("#page-2 h1", {
+  transform: "translateX(-130%)",
+  scrollTrigger: {
+    trigger: "#page-2",
+    scroller: "body",
+    markers: true,
+    start: "top 0%",
+    end: "top -200%",
+    scrub: 2,
+    pin: true,
+  },
+});
 
 gsap.from("#page-3 h1", {
   opacity: 0,
